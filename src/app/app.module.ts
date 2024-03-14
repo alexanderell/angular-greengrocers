121a7cb39a98f8a318d02d13d55385acc0bdb344
Alexander <alexander.ellnestam@gmail.com>
2024-03-14 11:21:17 +0100

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 import { StoreComponent } from './store/store.component';
